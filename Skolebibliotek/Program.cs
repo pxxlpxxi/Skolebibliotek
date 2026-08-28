9330559f7a5887571d8deb196b3d4105e78444a8
pxxlpxxi <vaikeflyger@hotmail.com>
2026-08-28 12:42:30 +0200

    Opdatering af Del 1: Fields & properties

--- Skolebibliotek/Program.cs
+++ Skolebibliotek/Program.cs
@@ -1,43 +1,114 @@
 ﻿/*
  1.
 
+
+Access modifiers:
+Alle fields private fordi det er best practice for fields (encapsulation).
+Properties bruges til at give adgang til fields udefra, så data kan valideres eller evt. ændres på en kontrolleret måde, f.eks. via et metodekald.
+
+
 BOOK
-Fields:
-- title
-- author
-- publicationYear
-- genre
-- ISBN
-- numberOfPages
-- language
-- publisher
-- isOnLoan
-- dueDate
+
+Title
+Field: private string _title
+Property: Title – kan læses og ændres udefra
+
+Author
+Field: private string _author
+Property: Author – kan læses og ændres udefra
+
+Publication Year
+Field: private int _publicationYear
+Property: PublicationYear – kan læses og ændres udefra
+
+Genre
+Field: private string _genre
+Property: Genre – kan læses og ændres udefra
+
+ISBN
+Field: private string _isbn
+Property: ISBN – kan læses udefra
+
+Number of Pages
+Field: private int _numberOfPages
+Property: NumberOfPages – kan læses udefra
+
+Language
+Field: private string _language
+Property: Language – kan læses og ændres udefra
+
+Publisher
+Field: private string _publisher
+Property: Publisher – kan læses og ændres udefra
+
+Is on Loan:
+Field: private bool _isOnLoan
+Property: IsOnLoan – kan læses udefra
+
+Due Date:
+Field: private DateTime _dueDate
+Property: DueDate – kan læses udefra
+
 
 BORROWER
-Fields:
-- name
-- address
-- phoneNumber
-- email
-- borrowerID
-- borrowedBooks (list of Book objects)
-- membershipDate
 
-Access modifiers: alle private fordi det er best practice for fields (encapsulation).
+Name
+Field: private string _name
+Property: Name – kan læses og ændres udefra
+
+Address
+Field: private string _address
+Property: Address – kan læses og ændres udefra
+
+Phone Number
+Field: private string _phoneNumber
+Property: PhoneNumber – kan læses og ændres udefra
+
+Email
+Field: private string _email
+Property: Email – kan læses og ændres udefra
+
+Borrower ID
+Field: private int _borrowerID
+Property: BorrowerID – kan læses udefra
+
 
 LIBRARY
 
-Books: list of Book objects
-Borrowers: list of Borrower objects
-BorrowedBooks: list of BorrowedBook objects
+Books
+Field: private List<Book> _books
+Property: Books – kan læses udefra
 
-BORROWEDBOOK
-Fields:
-- Book
-- Borrower
-- borrowDate
-- dueDate
+Borrowers
+Field : private List<Borrower> _borrowers
+Property: Borrowers – kan læses udefra
+
+BorrowedBooks
+Frield: private List<BorrowedBook> _borrowedBooks
+Property: BorrowedBooks – kan læses udefra
+
+Borrowed Books
+Field: private List<BorrowedBook> _borrowedBooks
+Property: BorrowedBooks – kan læses udefra
+
+
+BORROWED BOOK
+
+Book
+Field: private Book _book
+Property: Book – kan læses udefra
+
+Borrower
+Field: private Borrower _borrower
+Property: Borrower – kan læses udefra
+
+Borrow Date
+Field: private DateTime _borrowDate
+Property: BorrowDate – kan læses udefra
+
+Due Date
+Field: private DateTime _dueDate
+Property: DueDate – kan læses udefra
 
 
  */
