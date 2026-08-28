77b306cfe69b2d732fe65def37efcee611c5ae43
pxxlpxxi <vaikeflyger@hotmail.com>
2026-08-28 10:57:24 +0200

    Add project files.

---
+++ Skolebibliotek/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ 1.
+
+BOOK
+Fields:
+- title
+- author
+- publicationYear
+- genre
+- ISBN
+- numberOfPages
+- language
+- publisher
+- isOnLoan
+- dueDate
+
+BORROWER
+Fields:
+- name
+- address
+- phoneNumber
+- email
+- borrowerID
+- borrowedBooks (list of Book objects)
+- membershipDate
+
+Access modifiers: alle private fordi det er best practice for fields (encapsulation).
+
+LIBRARY
+
+Books: list of Book objects
+Borrowers: list of Borrower objects
+BorrowedBooks: list of BorrowedBook objects
+
+BORROWEDBOOK
+Fields:
+- Book
+- Borrower
+- borrowDate
+- dueDate
+
+
+ */
