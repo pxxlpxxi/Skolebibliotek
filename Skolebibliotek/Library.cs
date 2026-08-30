ff6d2154409d21c87b326991608e183ef4c20b37
pxxlpxxi <vaikeflyger@hotmail.com>
2026-08-30 21:50:04 +0200

    Added methods to Library.cs and updated error handling to fit assignment

--- Skolebibliotek/Library.cs
+++ Skolebibliotek/Library.cs
@@ -9,6 +9,10 @@
         private List<BorrowedBook> _borrowedBooks = new List<BorrowedBook>();
         internal IReadOnlyList<BorrowedBook> BorrowedBooks => _borrowedBooks.AsReadOnly();
 
+        internal (IReadOnlyList<Book> Books, IReadOnlyList<Borrower> Borrowers, IReadOnlyList<BorrowedBook> BorrowedBooks) ReadLibrary()
+        {
+            return (Books, Borrowers, BorrowedBooks);
+        }
 
         internal void CheckOut(Book book, Borrower borrower)
         {
@@ -25,10 +29,35 @@
             var borrowedBook = _borrowedBooks.FirstOrDefault(bb => bb.Book == book && bb.Borrower == borrower);
             if (borrowedBook == null)
             {
-                throw new InvalidOperationException("This book was not borrowed by this borrower.");
+                Console.WriteLine($"{book.Title} was not borrowed by {borrower.Name}.");
+                return;
             }
             book.Return();
             borrower.ReturnBook();
+            _borrowedBooks.Remove(borrowedBook);
+        }
+
+        internal void AddBook(Book book)
+        {
+            _books.Add(book);
+        }
+        internal void RemoveBook(Book book) { 
+            _books.Remove(book);
+        }
+        internal void AddBorrower(Borrower borrower)
+        {
+            _borrowers.Add(borrower);
+        }
+        internal void RemoveBorrower(Borrower borrower)
+        {
+            _borrowers.Remove(borrower);
+        }
+        internal void AddBorrowedBook(BorrowedBook borrowedBook)
+        {
+            _borrowedBooks.Add(borrowedBook);
+        }
+        internal void RemoveBorrowedBook(BorrowedBook borrowedBook)
+        {
             _borrowedBooks.Remove(borrowedBook);
         }
     }
