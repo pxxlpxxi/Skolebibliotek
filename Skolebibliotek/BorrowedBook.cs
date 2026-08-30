c7e7787da969325e73ceb2f5cff925e7827fc65b
pxxlpxxi <vaikeflyger@hotmail.com>
2026-08-30 21:51:07 +0200

    Added BorrowedBook.cs

---
+++ Skolebibliotek/BorrowedBook.cs
@@ -0,0 +1,23 @@
+﻿namespace Skolebibliotek
+{
+    internal class BorrowedBook
+    {
+        private Book _book;
+        internal Book Book => _book;
+        private Borrower _borrower;
+        internal Borrower Borrower => _borrower;
+        private DateTime _borrowDate;
+        internal DateTime BorrowDate => _borrowDate;
+        private DateOnly _dueDate;
+        internal DateOnly DueDate => _dueDate;
+
+        internal BorrowedBook(Book book, Borrower borrower, DateTime borrowDate, DateTime dueDate)
+        {
+            _book = book;
+            _borrower = borrower;
+            _borrowDate = borrowDate;
+            _dueDate = DateOnly.FromDateTime(dueDate);
+        }
+
+    }
+}
