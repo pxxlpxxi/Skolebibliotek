f4875a5a073e89aabd22dd968d515188d57e7d4e
pxxlpxxi <vaikeflyger@hotmail.com>
2026-08-30 21:50:37 +0200

    Added code to Program.cs

--- Skolebibliotek/Program.cs
+++ Skolebibliotek/Program.cs
@@ -1,3 +1,15 @@
-﻿/*
-Del 1 flyttet til README.md
- */
+﻿
+using Skolebibliotek;
+using System;
+internal class Program
+{
+    
+        static void Main(string[] args)
+        {
+            /*
+        Del 1 flyttet til README.md
+         */
+            Tests.RunAllTests();
+        }
+    
+}
